801775a5fe16c668c38a40a7d1a3e5f03bcc0e32
nebil esmael <nebilesmaelprog@gmail.com>
2025-10-21 03:54:27 +0300

    fixed layout folder

--- scooter/src/app/(store)/layout.tsx
+++ scooter/src/app/(store)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { Toaster } from 'sonner'
 import { CartProvider } from "@/contexts/cart-context"
 import NextAuthSessionProvider from '@/contexts/session-provider'
---
+++ scooter/src/app/layout.tsx
@@ -0,0 +1,15 @@
+
+import "./globals.css";
+
+export const metadata = {
+  title: "Lucky Liang Scooters",
+  description: "Premium E-Scooters in Dubai",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
